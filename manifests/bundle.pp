a430b46b3cc91093e3ca073f423a7acd6d9d8626
Vinicius Teles <vinicius@improveit.com.br>
2012-09-24 15:39:22 -0300

    Changed rbenv::bundle because I simply can't understand how to use the original one. :-/

--- manifests/bundle.pp
+++ manifests/bundle.pp
@@ -1,36 +1,17 @@
 define rbenv::bundle(
   $home,
   $user,
+  $path,
   $group   = $user,
-  $content = '',
-  $gems    = '',
 ) {
 
-  if ( $gems ) {
-    $gemfile = template('rbenv/Gemfile.erb')
-  } elsif ( $content ) {
-    $gemfile = $content
-  } else {
-    fail('bundle requires either a gem list or a Gemfile')
-  }
-
-  file {"${user}/Gemfile":
-    ensure  => present,
-    path    => "${home}/Gemfile",
-    owner   => $user,
-    group   => $group,
-    content => $gemfile,
-    backup  => false,
-    require => Rbenv::Client[$user],
-  }
-
   exec {"${user} bundle":
-    command   => "bundle --binstubs=${home}/bin --path=${home}/.bundle",
-    cwd       => $home,
+    command   => "bundle --path=${path}/.bundle",
+    cwd       => $path,
     user      => $user,
     group     => $group,
-    path      => "${home}/bin:${home}/.rbenv/shims:/bin:/usr/bin",
-    creates   => "${home}/Gemfile.lock",
-    subscribe => File["${user}/Gemfile"],
+    path      => "${home}/.rbenv/bin:${home}/.rbenv/shims:/bin:/usr/bin",
+    creates   => "${path}/Gemfile.lock",
+    logoutput => true,
   }
 }
