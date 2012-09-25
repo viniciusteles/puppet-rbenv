c90ddace9f4c52fed555aa85c400c1217f317feb
Vinicius Teles <vinicius@improveit.com.br>
2012-09-25 17:00:57 -0300

    Fix path to run bundle without problems

--- manifests/bundle.pp
+++ manifests/bundle.pp
@@ -7,12 +7,33 @@ define rbenv::bundle(
   $gems    = ''
 ) {
 
+Exec {
+  path => [
+           "${home}/.rbenv/bin",
+           "${home}/.rbenv/shims",
+           '/usr/local/sbin',
+           '/usr/local/bin',
+           '/usr/sbin',
+           '/usr/bin',
+           '/sbin',
+           '/bin'
+           ],
+}
   exec {"${user} bundle":
     command   => "bundle --path=${path}/.bundle",
     cwd       => $path,
     user      => $user,
     group     => $group,
-    path      => "${home}/.rbenv/bin:${home}/.rbenv/shims:/bin:/usr/bin",
+    path      => [
+                   "${home}/.rbenv/bin",
+                   "${home}/.rbenv/shims",
+                   '/usr/local/sbin',
+                   '/usr/local/bin',
+                   '/usr/sbin',
+                   '/usr/bin',
+                   '/sbin',
+                   '/bin'
+                  ],
     creates   => "${path}/Gemfile.lock",
     timeout   => 0,
     logoutput => true,
