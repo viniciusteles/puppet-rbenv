c72185f7dccdaa0c3dc47758963e4d6a125c4cf6
Vinicius Teles <vinicius@improveit.com.br>
2012-09-25 18:39:14 -0300

    Fixing the exec PATH again

--- manifests/bundle.pp
+++ manifests/bundle.pp
@@ -7,20 +7,8 @@ define rbenv::bundle(
   $gems    = ''
 ) {
 
-Exec {
-  path => [
-           "${home}/.rbenv/bin",
-           "${home}/.rbenv/shims",
-           '/usr/local/sbin',
-           '/usr/local/bin',
-           '/usr/sbin',
-           '/usr/bin',
-           '/sbin',
-           '/bin'
-           ],
-}
   exec {"${user} bundle":
-    command   => "bundle --path=${path}/.bundle",
+    command   => "rbenv exec bundle --path=${path}/.bundle",
     cwd       => $path,
     user      => $user,
     group     => $group,
